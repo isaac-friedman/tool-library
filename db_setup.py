58fcce71bbf520b5dc505889ddadd8befd0d04be
Isaac Friedman <isaac@isaac-friedman.com>
2019-07-02 19:45:25 +0000

    fix: relationship mappings in Tool model

--- db_setup.py
+++ db_setup.py
@@ -17,14 +17,22 @@ class Tool(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
     description = db.Column(db.String(300))
+    '''
+    Possible values:
+        1- home
+        2- work
+        3- on loan
+        4- unknown
+    '''
     location = db.Column(db.Integer)
     notes = db.Column(db.String(300))
     # All tools must belong to a user.
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False,
-        backref=db.backref('tools'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('tools'))
     # All tools must belong to a category.
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False,
-        backref=db.backref('tools'))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'),
+    nullable=False)
+    category = db.relationship('Category', backref=db.backref('tools'))
 
 
 class Category(db.Model):
