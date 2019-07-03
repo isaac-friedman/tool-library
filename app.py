3306218a79fa4f03bc86fd683cbf8d576d9b4340
Isaac Friedman <isaac@isaac-friedman.com>
2019-07-03 00:52:26 +0000

    Add main app object definition

--- app.py
+++ app.py
@@ -2,28 +2,41 @@ from flask import Flask, request, render_template, url_for, redirect, flash, jso
 
 from config import Config
 
+app = Flask(__name__)
 
 @app.route('/')
 @app.route('/login/')
 def login():
     return "This will be the login page."
 
+
 @app.route('/tools/')
 def all():
     return "This page will list all of the logged-in user's tools."
+
 
 @app.route('/tools/<int:category_id>/')
 def list_category(category_id):
     return "This page will list all of a user's tools of the specified category."
 
+
 @app.route('/tools/new')
 def new():
     return "Future New Tool Form"
+
 
 @app.route('/tools/<int:tool_id>/edit')
 def edit_tool(tool_id):
     return "Future Edit form for existing tools"
 
-@app.route('/tools/<int:tool_id/delete')
+
+@app.route('/tools/<int:tool_id>/delete')
 def delete(tool_id):
     return "Future delete form for existing tools"
+
+
+if __name__ == '__main__':
+    app.secret_key = '''One day a randomly generated 128-bit string
+      will go here. Well, not *here* here because we'll use OAuth'''
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
