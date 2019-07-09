16be5193d6602b01d5a8a48e40a52c00476c41c7
Isaac Friedman <isaac@isaac-friedman.com>
2019-07-09 14:57:13 +0000

    Add sample super user and sample tools

--- sample_data.py
+++ sample_data.py
@@ -1,4 +1,6 @@
-from db_setup import db, Category, User, Tool
+from app import db, Category, User, Tool
+
+super_user = User(firstname='Isaac', lastname='Friedman', email='isaac@isaac-friedman.com')
 
 cats = [
     Category(name='Hand Tools', description="Miscellaneous non-powered, one-person tools"),
@@ -8,7 +10,23 @@ cats = [
     Category(name='Welding', description="Hot metal glue gun and accessories")
 ]
 
+tools = [
+    Tool(name='Framing Hammer', description='18oz straight claw hammer',
+        category_id=2, user_id=1),
+    Tool(name='6-in-1 screwdriver',
+        description='2 sizes each, philips and flat + nut drivers',
+        location=2, category_id=1, user_id=1),
+    Tool(name='Vise Grips',
+        description="You don't need one until you wish you had one.",
+        category_id=1, user_id=1)
+]
+"""
 for cat in cats:
     db.session.add(cat)
+"""
+db.session.add(super_user)
+
+for tool in tools:
+    db.session.add(tool)
 
 db.session.commit()
