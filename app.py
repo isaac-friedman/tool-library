566d5490b9c25a4112366bb34a334e7ce2ca9f58
Isaac Friedman <isaac@isaac-friedman.com>
2019-07-12 02:26:02 +0000

    feat: Login button frontend

--- app.py
+++ app.py
@@ -83,10 +83,9 @@ def login():
     state = ''.join(random.choice(string.ascii_uppercase
                 + string.ascii_lowercase
                 + string.digits) for x in range(64))
-    print(state) # Debug. Remove me
+    print(state)  # Debug. Remove me
     login_session['state'] = state
-    print(login_session['state'])# Debug. Remove me
-    return "The current state of the login session is {0}".format(login_session['state'])
+    print(login_session['state'])  # Debug. Remove me
     return render_template("login.html")
 
 
