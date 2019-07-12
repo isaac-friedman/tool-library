420de2e43f3f3e8940f488b734ed56aed90e9d63
Isaac Friedman <isaac@isaac-friedman.com>
2019-07-12 19:06:07 +0000

    Upgrade auth to current google signin

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask, request, render_template, url_for, redirect, flash, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask import session as login_session
 import random
 import string
+import json
+import httplib2
+import requests
+
+from flask import Flask, request, render_template, url_for, redirect, flash, \
+    jsonify, make_response, session as login_session
+from flask_sqlalchemy import SQLAlchemy
+
+from oauth2client.client import flow_from_clientsecrets, FlowExchangeError
+
+
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Tool Trackr"
 
 
 app = Flask(__name__)
@@ -46,7 +56,7 @@ class Tool(db.Model):
     user = db.relationship('User', backref=db.backref('tools'))
     # All tools must belong to a category.
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'),
-    nullable=False)
+                            nullable=False)
     category = db.relationship('Category', backref=db.backref('tools'))
 
     @property
@@ -81,18 +91,104 @@ class Category(db.Model):
 # Create a state token and store it in the session for validation
 def login():
     state = ''.join(random.choice(string.ascii_uppercase
-                + string.ascii_lowercase
-                + string.digits) for x in range(64))
-    print(state)  # Debug. Remove me
+                    + string.ascii_lowercase
+                    + string.digits) for x in range(64))
     login_session['state'] = state
-    print(login_session['state'])  # Debug. Remove me
     return render_template("login.html", STATE=state)
+
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    print("Entered gconnect function")
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+    print ("You have a code of: {0}".format(code))
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
 
 
 @app.route('/tools/')
 def all():
     tools = db.session.query(Tool.id, Tool.name, Tool.description,
-      Category.name).join(Category, Tool.category_id == Category.id).all()
+                             Category.name).join(Category,
+                                                 Tool.category_id ==
+                                                 Category.id).all()
     by_category = {}
     for tool in tools:
         if tool[3] not in by_category:
@@ -128,11 +224,11 @@ def new():
     if request.method == 'POST':
         # Use of user_id from input is strictly temporary
         newTool = Tool(user_id=int(request.form['user_id']),
-          name=request.form['name'],
-          description=request.form['description'],
-          location=int(request.form['location']),
-          notes=request.form['notes'],
-          category_id=int(request.form['category']))
+                       name=request.form['name'],
+                       description=request.form['description'],
+                       location=int(request.form['location']),
+                       notes=request.form['notes'],
+                       category_id=int(request.form['category']))
         db.session.add(newTool)
         db.session.commit()
         return redirect(url_for('all'))
