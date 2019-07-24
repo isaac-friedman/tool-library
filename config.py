6b1c082cae3bd6448725d1ad3922d75605198654
Isaac Friedman <isaac@isaac-friedman.com>
2019-07-24 01:22:06 +0000

    Remove unused files

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config(object):
-    """docstring for Config."""
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "There will be a 128-bit random string here at some point"
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///catalog.db'
-    TESTING = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
